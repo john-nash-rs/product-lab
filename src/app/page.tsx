1da6e9fbd9f23e804a91a6df524f643b61168614
john-nash-rs <rohitsingh20121992@gmail.com>
2024-01-18 21:45:17 +0530

    making a bit pretty

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
 "use client"
-
-import MediaPlayer from '@/components/MediaPlayer'
-import Suggestion from '@/components/Suggestion'
+import React from 'react';
+import MediaPlayer from '@/components/MediaPlayer';
+import Suggestion from '@/components/Suggestion';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center p-24">
-      <div className="flex">
-        <div className="mr-8">
-          <MediaPlayer/>
+    <div className="bg-gray-900 text-white min-h-screen">
+      <header className="p-4 text-center">
+        <h1 className="text-3xl font-bold">Stream Video</h1>
+      </header>
+      <main className="flex justify-center items-center p-8">
+        <div className="flex">
+          <div className="mr-8">
+            <MediaPlayer />
+          </div>
+          <div className="flex-grow">
+            <Suggestion />
+          </div>
         </div>
-        <div className="flex-grow">
-          <Suggestion/>
-        </div>
-      </div>
-    </main>
-  )
+      </main>
+      <footer className="text-center py-4 bg-gray-800">
+        <p>&copy; 2024 Stream Video. All rights reserved.</p>
+      </footer>
+    </div>
+  );
 }
